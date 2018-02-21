84e8e73f76f182bfc558f052c58f39b7f9eb63d2
mhf <mhfcbz@163.com>
2018-02-21 16:28:03 +0800

    fix:

--- src/conf/routes.js
+++ src/conf/routes.js
@@ -1,7 +1,11 @@
+import NotFound from "../root/NotFound.vue"
 import Main from "../root/main/a.vue"
 import About from "../root/about/a.vue"
 
 export default [
   { path: "/", component: Main },
+
   { path: "/about", component: About },
+
+  { path: "*", component: NotFound },
 ]
---
+++ src/root/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Page Not Found</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+  .green {
+    color: #0f0;
+  }
+</style>
