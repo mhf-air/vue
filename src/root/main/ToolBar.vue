b054012019b81823186793811377d16a0868927f
mhf <mhfcbz@163.com>
2018-02-22 19:17:11 +0800

    fix:

--- src/root/main/ToolBar.vue
+++ src/root/main/ToolBar.vue
@@ -1,8 +1,16 @@
 <template>
-  <div>
-    <p>{{ age }}</p>
-    <input v-model.number="age" type="number"/>
-  </div>
+  <header>
+    <img src="image/lion.svg" alt="logo"/>
+    <ul class="header-item-group">
+      <li class="header-item">发现音乐</li>
+      <li class="header-item">我的音乐</li>
+      <li class="header-item">朋友</li>
+      <li class="header-item">商城</li>
+      <li class="header-item">音乐人</li>
+      <li class="header-item">下载客户端</li>
+    </ul>
+    <input type="search" placeholder="enter sth"/>
+  </header>
 </template>
 
 <script>
@@ -16,7 +24,26 @@ export default {
 </script>
 
 <style scoped>
-  .green {
-    color: #0f0;
+  header {
+    display: flex;
+    font-size: 1.3rem;
+    height: 5.9rem;
+    color: white;
+    background-color: #292929;
+  }
+
+  .header-item-group {
+    display: flex;
+    list-style-type: none;
+  }
+
+  .header-item {
+    justify-content: space-around;
+  }
+
+  input[type="search"] {
+    height: 50%;
+    border-radius: 5rem;
+    border-color: #0f0;
   }
 </style>
