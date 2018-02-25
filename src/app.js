2039d47373e03145f82433d83c74bcd645148ae0
mhf <mhfcbz@163.com>
2018-02-25 17:43:47 +0800

    fix:

--- src/app.js
+++ src/app.js
@@ -19,6 +19,11 @@ const router = new VueRouter({
   mode: "history",
 })
 
+// element-ui
+import ElementUI from "element-ui"
+// import "element-ui/lib/theme-chalk/index.css"
+Vue.use(ElementUI)
+
 // root
 import App from "./root/a.vue"
 new Vue({
--- src/root/main/test.vue
+++ src/root/main/test.vue
@@ -8,6 +8,10 @@
       <router-link to="/profile/info">Go to Profile Info</router-link>
     </p>
 
+    <el-badge :value="12">
+      <el-button>评论</el-button>
+    </el-badge>
+
     <label>enter todo</label>
     <input @keyup.enter="addTodo"/>
     <ol>
