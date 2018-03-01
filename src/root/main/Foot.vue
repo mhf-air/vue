f982e099669918c3bb6a87d1c6e80034ebeb0cf2
mhf <mhfcbz@163.com>
2018-03-01 19:42:45 +0800

    fix:

--- src/root/main/Foot.vue
+++ src/root/main/Foot.vue
@@ -1,8 +1,8 @@
 <template>
 <el-footer class="page-footer">
-  <div id="footer-top">
+  <nav id="footer-top">
     <div class="footer-col">
-      <div>关于</div>
+      <h3>关于</h3>
       <ul>
         <li>
           <router-link to="">招聘信息</router-link>
@@ -17,7 +17,7 @@
     </div>
 
     <div class="footer-col">
-      <div>服务</div>
+      <h3>服务</h3>
       <ul>
         <li>
           <router-link to="">到达和出发</router-link>
@@ -32,7 +32,7 @@
     </div>
 
     <div class="footer-col">
-      <div>客户支持</div>
+      <h3>客户支持</h3>
       <ul>
         <li>
           <router-link to="">联系方式</router-link>
@@ -46,17 +46,27 @@
       </ul>
     </div>
 
-    <div>
-      <div>电子通讯</div>
-      <div>注册</div>
+    <div class="footer-col">
+      <h3>电子通讯</h3>
+      <div id="register">
+        <router-link to="">注册</router-link>
+      </div>
     </div>
-  </div>
+  </nav>
 
   <div id="footer-bottom">
     <span>© Swiss International Air Lines</span>
-    <router-link to="">汉莎航空集团</router-link>
-    <router-link to="">网站地图</router-link>
-    <router-link to="">法规</router-link>
+    <ul>
+      <li>
+        <router-link to="">汉莎航空集团</router-link>
+      </li>
+      <li>
+        <router-link to="">网站地图</router-link>
+      </li>
+      <li>
+        <router-link to="">法规</router-link>
+      </li>
+    </ul>
   </div>
 </el-footer>
 </template>
@@ -74,21 +84,60 @@ export default {
 <style scoped>
 .page-footer {
   display: flex;
-  flex-flow: column;
-  flex: 1 10rem;
+  flex-flow: wrap;
+  flex: 1 20rem;
+  justify-content: center;
   width: 100%;
-  background-color: cyan;
+  padding: 0;
+  /* background-color: cyan; */
+}
+
+.page-footer ul {
+  list-style: none;
 }
 
 #footer-top {
-  background-color: red;
   display: flex;
   flex-flow: wrap;
+  justify-content: center;
+}
+
+.footer-col {
+  margin-right: 1rem;
+}
+
+.footer-col>ul {
+  width: 20rem;
+  padding: 0;
+}
+
+.footer-col>ul>li {
+  width: 100%;
+  padding: 0.3rem 0;
+  border-top: 1px solid #d6d3cc;
+}
+
+.footer-col>ul:last-child {
+  border-bottom: 1px solid #d6d3cc;
+}
+
+#register {
+  padding: 0.5rem;
+  width: 15rem;
+  border: 1px solid #999999;
 }
 
 #footer-bottom {
-  background-color: yellow;
   display: flex;
   flex-flow: wrap;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  background-color: rgb(237, 236, 232);
+}
+
+#footer-bottom>ul>li {
+  display: inline-block;
+  margin-right: 1rem;
 }
 </style>
