d0be9a3380bd87d24d867e9a6e8297c23ea5eb2b
mhf <mhfcbz@163.com>
2018-03-06 20:21:36 +0800

    fix:

--- src/root/main/SiteHeader.vue
+++ src/root/main/SiteHeader.vue
@@ -1,8 +1,20 @@
 <template>
 <header id="site-header">
-  <div id="site-header-top">
-    上
-  </div>
+  <g-h justify-content="space-between" id="site-header-top">
+    <g-h align-items="center" id="site-header-top-left">
+      <router-link to="">小米商城</router-link>
+      <router-link to="">MIUI</router-link>
+      <router-link to="">IoT</router-link>
+      <router-link to="">云服务</router-link>
+      <router-link to="">水滴</router-link>
+      <router-link to="">金融</router-link>
+      <router-link to="">有品</router-link>
+      <router-link to="">Select Region</router-link>
+    </g-h>
+    <div id="site-header-top-right">
+      上右
+    </div>
+  </g-h>
   <div id="site-header-bottom">
     下
   </div>
@@ -32,6 +44,32 @@ export default {
   color: #b0b0b0;
   padding: 0 19.3rem;
   font-size: 12px;
+}
+
+/* top left */
+
+#site-header-top-left {
+  padding: 0.4rem 0;
+}
+
+#site-header-top-left>* {
+  color: #b0b0b0;
+  padding-right: 0.6rem;
+}
+
+#site-header-top-left>*:hover {
+  color: white;
+}
+
+#site-header-top-left>*:not(:last-child)::after {
+  content: " |";
+  /* color: #424242; */
+}
+
+/* top right */
+
+#site-header-top-right {
+  background: cyan;
 }
 
 /* bottom */
