59aaeff0344236fcb19b2038ef7ce09904abcc75
mhf <mhfcbz@163.com>
2018-03-07 22:31:17 +0800

    fix:

--- src/root/main/SiteHeader.vue
+++ src/root/main/SiteHeader.vue
@@ -1,18 +1,19 @@
 <template>
 <header class="site-header">
-  <g-h justify-content="space-between" class="site-header-top">
-    <g-h align-items="center" class="site-header-top-left">
-      <router-link to="">小米商城</router-link>
-      <router-link to="">MIUI</router-link>
-      <router-link to="">IoT</router-link>
-      <router-link to="">云服务</router-link>
-      <router-link to="">水滴</router-link>
-      <router-link to="">金融</router-link>
-      <router-link to="">有品</router-link>
-      <router-link to="">Select Region</router-link>
-    </g-h>
-    <g-h justify-content="flex-end" class="site-header-top-right">
-      <!-- <el-dropdown placement="bottom" class="site-header-top-right-user">
+  <g-h justify-content="center" class="site-header-top">
+    <g-h justify-content="space-between" class="site-header-content">
+      <g-h align-items="center" class="site-header-top-left">
+        <router-link to="">小米商城</router-link>
+        <router-link to="">MIUI</router-link>
+        <router-link to="">IoT</router-link>
+        <router-link to="">云服务</router-link>
+        <router-link to="">水滴</router-link>
+        <router-link to="">金融</router-link>
+        <router-link to="">有品</router-link>
+        <router-link to="">Select Region</router-link>
+      </g-h>
+      <g-h justify-content="flex-end" class="site-header-top-right">
+        <!-- <el-dropdown placement="bottom" class="site-header-top-right-user">
         <span class="el-dropdown-link">
           {{ username }}
           <i class="el-icon-arrow-down el-icon-right"></i>
@@ -25,26 +26,50 @@
           <el-dropdown-item>退出登录</el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown> -->
-      <div class="site-header-top-right-user">
-        <span>
+        <div class="site-header-top-right-user">
+          <span>
           {{ username }}
-          <i class=" el-icon-arrow-down el-icon-right "></i>
+          <i class="el-icon-arrow-down el-icon-right"></i>
         </span>
-        <div class="popover">
-          <div>个人中心</div>
-          <div>评价晒单</div>
-          <div>我的喜欢</div>
-          <div>小米账户</div>
-          <div>退出登录</div>
+          <div class="popover">
+            <div>个人中心</div>
+            <div>评价晒单</div>
+            <div>我的喜欢</div>
+            <div>小米账户</div>
+            <div>退出登录</div>
+          </div>
         </div>
-      </div>
-      <router-link to="">消息通知</router-link>
-      <router-link to="">我的订单</router-link>
+        <router-link to="">消息通知</router-link>
+        <router-link to="">我的订单</router-link>
+      </g-h>
     </g-h>
   </g-h>
-  <div class="site-header-bottom ">
-    下
-  </div>
+  <g-h justify-content="center" class="site-header-bottom">
+    <g-h justify-content="space-between" align-items="center" class="site-header-content">
+      <g-h justify-content="space-between" class="site-header-bottom-left">
+        <router-link to="" class="site-header-bottom-logo"></router-link>
+        <router-link to="">小米手机</router-link>
+        <router-link to="">红米</router-link>
+        <router-link to="">电视</router-link>
+        <router-link to="">笔记本</router-link>
+        <router-link to="">盒子</router-link>
+        <router-link to="">新品</router-link>
+        <router-link to="">路由器</router-link>
+        <router-link to="">智能硬件</router-link>
+        <router-link to="">服务</router-link>
+        <router-link to="">社区</router-link>
+      </g-h>
+      <div class="site-header-bottom-right">
+        <el-input>
+          <g-h align-items="center" slot="suffix" class="site-header-bottom-right-suffix">
+            <span>abc</span>
+            <span>abc</span>
+          </g-h>
+          <el-button slot="append" icon="el-icon-search" />
+        </el-input>
+      </div>
+    </g-h>
+  </g-h>
 </header>
 </template>
 
@@ -74,8 +99,11 @@ export default {
 .site-header-top {
   background-color: #363636;
   color: #b0b0b0;
-  padding: 0 19.3rem;
   font-size: 12px;
+}
+
+.site-header-content {
+  width: 77rem;
 }
 
 /* top left */
@@ -150,7 +178,25 @@ export default {
 /* bottom */
 
 .site-header-bottom {
-  padding: 0 19.3rem;
   font-size: 16px;
+  padding: 2.3rem 0;
+  background: cyan;
+}
+
+.site-header-bottom-left>* {
+  margin-right: 1rem;
+  color: #333;
+}
+
+.site-header-bottom-left>*:hover {
+  color: #ff6700;
+}
+
+.site-header-bottom-logo {
+  background: url("/image/search.png");
+}
+
+.site-header-bottom-right-suffix {
+  background: cyan;
 }
 </style>
