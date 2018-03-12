59563a0e581b70706b5362237ef7e98f090554de
mhf <mhfcbz@163.com>
2018-03-12 12:32:42 +0800

    s:

--- src/root/main/Content.vue
+++ src/root/main/Content.vue
@@ -16,14 +16,16 @@
             <span>{{ item.name }}</span>
             <span>&gt;</span>
           </g-h>
-          <g-v wrap class="section-main-product-nav-popover">
-            <div v-for="(popItem, j) in item.list" :key="j" class="section-main-product-nav-popover-popitem">
-              <router-link to="popItem.link">
-                <img :src="popItem.src" />
-                <span>{{ popItem.name }}</span>
-              </router-link>
-            </div>
-          </g-v>
+          <div class="section-main-product-nav-popover">
+            <g-h wrap>
+              <div v-for="(popItem, j) in item.list" :key="j" class="section-main-product-nav-popover-popitem">
+                <router-link to="popItem.link">
+                  <img :src="popItem.src" />
+                  <span>{{ popItem.name }}</span>
+                </router-link>
+              </div>
+            </g-h>
+          </div>
         </g-h>
       </g-v>
     </div>
@@ -32,6 +34,50 @@
 </template>
 
 <script>
+let mainProductNavList_less = [
+  { src: "", to: "", name: "小米Note 3" },
+  { src: "", to: "", name: "小米Mix 2" },
+  { src: "", to: "", name: "小米6" },
+  { src: "", to: "", name: "小米5X" },
+  { src: "", to: "", name: "小米Max 2" },
+  { src: "", to: "", name: "红米5 Plus" },
+  { src: "", to: "", name: "红米5" },
+  { src: "", to: "", name: "红米5A" },
+  { src: "", to: "", name: "红米Note 5A" },
+  { src: "", to: "", name: "红米Note 4X" },
+  { src: "", to: "", name: "红米4A" },
+  { src: "", to: "", name: "移动4G+专区" },
+  { src: "", to: "", name: "对比手机" },
+  { src: "", to: "", name: "米粉卡 日租卡" },
+  { src: "", to: "", name: "小米移动 电话卡" },
+  { src: "", to: "", name: "手机上门维修" },
+]
+
+let mainProductNavList_more = [
+  { src: "", to: "", name: "红米4A" },
+  { src: "", to: "", name: "移动4G+专区" },
+  { src: "", to: "", name: "对比手机" },
+  { src: "", to: "", name: "米粉卡 日租卡" },
+  { src: "", to: "", name: "小米移动 电话卡" },
+  { src: "", to: "", name: "手机上门维修" },
+  { src: "", to: "", name: "小米Note 3" },
+  { src: "", to: "", name: "小米Mix 2" },
+  { src: "", to: "", name: "小米6" },
+  { src: "", to: "", name: "小米5X" },
+  { src: "", to: "", name: "小米Max 2" },
+  { src: "", to: "", name: "红米5 Plus" },
+  { src: "", to: "", name: "红米5" },
+  { src: "", to: "", name: "红米5A" },
+  { src: "", to: "", name: "红米Note 5A" },
+  { src: "", to: "", name: "红米Note 4X" },
+  { src: "", to: "", name: "红米4A" },
+  { src: "", to: "", name: "移动4G+专区" },
+  { src: "", to: "", name: "对比手机" },
+  { src: "", to: "", name: "米粉卡 日租卡" },
+  { src: "", to: "", name: "小米移动 电话卡" },
+  { src: "", to: "", name: "手机上门维修" },
+]
+
 export default {
   data() {
     return {
@@ -45,32 +91,43 @@ export default {
       mainProductNavList: [
         {
           name: "手机　电话卡",
-          list: [
-            { src: "", to: "", name: "小米Note 3" },
-            { src: "", to: "", name: "小米Mix 2" },
-            { src: "", to: "", name: "小米6" },
-            { src: "", to: "", name: "小米5X" },
-            { src: "", to: "", name: "小米Max 2" },
-            { src: "", to: "", name: "红米5 Plus" },
-            { src: "", to: "", name: "红米5" },
-            { src: "", to: "", name: "红米5A" },
-            { src: "", to: "", name: "红米Note 5A" },
-            { src: "", to: "", name: "红米Note 4X" },
-            { src: "", to: "", name: "红米4A" },
-            { src: "", to: "", name: "移动4G+专区" },
-            { src: "", to: "", name: "对比手机" },
-            { src: "", to: "", name: "米粉卡 日租卡" },
-            { src: "", to: "", name: "小米移动 电话卡" },
-            { src: "", to: "", name: "手机上门维修" },
-          ],
+          list: mainProductNavList_less,
+        },
+        {
+          name: "电视 盒子",
+          list: mainProductNavList_more,
         },
         {
           name: "笔记本",
-          list: [
-            { src: "", to: "", name: "笔记本" },
-            { src: "", to: "", name: "笔记本" },
-            { src: "", to: "", name: "笔记本" },
-          ],
+          list: mainProductNavList_less,
+        },
+        {
+          name: "智能　家电",
+          list: mainProductNavList_more,
+        },
+        {
+          name: "健康　家居",
+          list: mainProductNavList_less,
+        },
+        {
+          name: "出行　儿童",
+          list: mainProductNavList_more,
+        },
+        {
+          name: "路由器　手机配件",
+          list: mainProductNavList_less,
+        },
+        {
+          name: "移动电源　插线板",
+          list: mainProductNavList_more,
+        },
+        {
+          name: "耳机　音箱",
+          list: mainProductNavList_less,
+        },
+        {
+          name: "生活　米兔",
+          list: mainProductNavList_more,
         },
       ],
     }
@@ -110,15 +167,18 @@ primary = #ff6700
     display: none
     position: absolute
     top: 0
-    right: -50rem
+    right: -62rem
     bottom: 0
     left: 100%
-    z-index: 2
-    background: #fff
-    box-shadow: 0 3px 10px #ccc
+    z-index: 3
+    &>.g-h
+      writing-mode: vertical-lr
+      background: #fff
+      box-shadow: 0 3px 10px #ccc
     &-popitem
       padding: 1.988em 3em
       color: #000
+      writing-mode: horizontal-tb
       span:hover
         color: primary
 </style>
