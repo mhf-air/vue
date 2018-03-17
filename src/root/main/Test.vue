0581c01f2f7cc926e5c314941b1116a6e5206389
mhf <mhfcbz@163.com>
2018-03-17 20:33:56 +0800

    s:

--- src/root/main/Test.vue
+++ src/root/main/Test.vue
@@ -1,23 +1,5 @@
 <template lang="pug">
-g-v(align-items="center" class="nice")
-  //- main product
-  div(class="main-product")
-    el-carousel(height="28rem")
-      el-carousel-item(v-for="(item, i) in mainProductList" :key="i")
-        router-link(to="")
-          img(:src="item.src" :alt="item.alt")
-    g-v(justify-content="center" class="main-product-nav")
-      g-h(justify-content="center" class="main-product-nav")
-        g-h(justify-content="space-between")
-          span {{ item.name }}
-          span &gt;
-        div(class="main-product-nav-popover")
-          g-h(wrap)
-            div(v-for="(popItem, j) in item.list" :key="j" class="main-product-nav-popover-popitem")
-              router-link(to="popItem.link")
-                img(:src="popItem.link")
-                span {{ popItem.name }}
-
+div nice
 </template>
 
 <script>
--- src/root/main/a.vue
+++ src/root/main/a.vue
@@ -1,10 +1,9 @@
-<template>
-<div>
-  <SiteHeader/>
-  <!-- <Test/> -->
-  <Content/>
-  <SiteFooter/>
-</div>
+<template lang="pug">
+div
+  SiteHeader
+  Test
+  Content
+  SiteFooter
 </template>
 
 <script>
@@ -19,6 +18,6 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
 
 </style>
