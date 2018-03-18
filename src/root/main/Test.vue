bae42c987895d6434a35f2f62e9da374942d7c86
mhf <mhfcbz@163.com>
2018-03-18 19:56:08 +0800

    s:

--- src/root/main/Test.vue
+++ src/root/main/Test.vue
@@ -4,10 +4,10 @@ div(class="app")
     div(@mouseenter="showPopover" @mouseleave="hidePopover") Toggle
     transition(name="fade")
       div(v-if="show" class="popover")
-        ul
-          li alice
-          li bob
-          li cat
+        g-v
+          div alice
+          div bob
+          div cat
 </template>
 
 <script>
@@ -29,25 +29,39 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.fade-enter-active
-.fade-leave-active
-  transition: all 0.5s
+// .fade-enter-active
+// .fade-leave-active
+  // animation: drop-down 0.5s
 
-.fade-enter
-.fade-leave-to
-  transform: translateY(0)
+// .fade-enter
+// .fade-leave-to
+  // transform: translateY(0)
 
 .app
   background: red
 
 .popover-container
   position: relative
+  &:hover .popover
+    animation: drop-down 0.1s
 
 .popover
   position: absolute
   top: 100%
   left: 0
   background: cyan
-  overflow: hidden
+  transform-origin: center top
+
+@keyframes drop-down
+  0%
+    transform: scaleY(0)
+  25%
+    transform: scaleY(0.25)
+  50%
+    transform: scaleY(0.5)
+  75%
+    transform: scaleY(0.75)
+  100%
+    transform: scaleY(1)
 
 </style>
