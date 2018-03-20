311ce3ac4cf9be20c2e0edb839b041de8bd97c01
mhf <mhfcbz@163.com>
2018-03-20 13:52:54 +0800

    s:

--- src/root/main/Test.vue
+++ src/root/main/Test.vue
@@ -1,13 +1,12 @@
 <template lang="pug">
 div(class="app")
   div(class="popover-container")
-    div(@mouseenter="showPopover" @mouseleave="hidePopover") Toggle
-    transition(name="fade")
-      div(v-if="show" class="popover")
-        g-v
-          div alice
-          div bob
-          div cat
+    div Toggle
+    div(class="popover")
+      g-v
+        div alice
+        div bob
+        div cat
 </template>
 
 <script>
@@ -18,32 +17,18 @@ export default {
     }
   },
   methods: {
-    showPopover() {
-      this.show = true
-    },
-    hidePopover() {
-      this.show = false
-    },
   },
 }
 </script>
 
 <style lang="stylus" scoped>
-// .fade-enter-active
-// .fade-leave-active
-  // animation: drop-down 0.5s
-
-// .fade-enter
-// .fade-leave-to
-  // transform: translateY(0)
-
 .app
   background: red
 
 .popover-container
   position: relative
   &:hover .popover
-    animation: drop-down 0.1s
+    transform: scale(1, 1)
 
 .popover
   position: absolute
@@ -51,17 +36,7 @@ export default {
   left: 0
   background: cyan
   transform-origin: center top
-
-@keyframes drop-down
-  0%
-    transform: scaleY(0)
-  25%
-    transform: scaleY(0.25)
-  50%
-    transform: scaleY(0.5)
-  75%
-    transform: scaleY(0.75)
-  100%
-    transform: scaleY(1)
+  transform: scale(1, 0)
+  transition: transform 100ms ease-in
 
 </style>
